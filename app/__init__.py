8ddf5754c65a294e352c87d6cb9c8fd50f8633fc
WoobieApps <piotrasel@gmail.com>
2023-11-06 18:36:54 +0100

    ADD base auth and html layout

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_utils import database_exists, create_database
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 db_url = "postgresql://postgres:postgres@localhost:5432/HomeBudget"
 
@@ -9,8 +11,10 @@ if not database_exists(db_url):
 
 app = Flask(__name__, template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = db_url
-
+app.config['SECRET_KEY'] = '2a2f075acf6a18bd4612bf420066a533'
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from app.routes import routes
