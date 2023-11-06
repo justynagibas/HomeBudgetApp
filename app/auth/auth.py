ef09d9ed82dfc19ed87f705896a771e2d40c94b3
Piotr Łuba <piotrasel@gmail.com>
2023-11-06 19:59:25 +0100

    ADD base auth and html layout (#13)
    * ADD base auth and html layout
    
    * FIX Username constraints in login form

---
+++ app/auth/auth.py
@@ -0,0 +1,18 @@
+from app import login_manager, bcrypt, db
+from app.database.database import Users
+
+def insert_user(username, password):
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+    user = Users(user_name=username, password=hashed_password)
+    db.session.add(user)
+    db.session.commit()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
+def check_user_credentials(username, password):
+    user = Users.query.filter_by(user_name=username).first()
+    if user and bcrypt.check_password_hash(user.password, password):
+        return user
+    return False
---
+++ app/auth/auth_forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
+from app.database.database import Users
+
+class SingupForm(FlaskForm):
+    login = StringField('Your login (at least 3 characters long)', validators=[DataRequired(), Length(min=4, max=15)])
+    password = PasswordField('Your password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up!')
+
+    def validate_login(self, login):
+        user = Users.query.filter_by(user_name=login.data).first()
+        if user:
+            raise ValidationError('This login is already taken. Please choose another one.')
+
+
+class LoginForm(FlaskForm):
+    login = StringField('Your login', validators=[DataRequired()])
+    password = PasswordField('Your password', validators=[DataRequired()])
+    submit = SubmitField('Log in')
