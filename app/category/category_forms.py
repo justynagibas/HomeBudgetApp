7ca29e70dfc5851aad001ea0996b45714542d4ef
justynagibas <juztynagibas@gmail.com>
2023-12-15 12:43:16 +0100

    Manage category and subcategory.

---
+++ app/category/category_forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField, DateField, SubmitField, SelectField, HiddenField
+from wtforms.validators import DataRequired, Length, InputRequired, Optional, NumberRange
+
+
+class AddCategoryForm(FlaskForm):
+    category_name = StringField("Category name:", validators=[DataRequired(), Length(max=100)])
+    submit = SubmitField("Submit")
+
+
+class RemoveCategoryForm(FlaskForm):
+    category_name = SelectField("Category name:", choices=[], validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+class AddSubcategoryForm(FlaskForm):
+    subcategory_name = StringField("Subcategory name:", validators=[DataRequired(), Length(max=100)])
+    category_name = HiddenField("Category name:", validators=[DataRequired(), Length(max=100)])
+    submit = SubmitField("Submit")
+
+
+class RemoveSubcategoryForm(FlaskForm):
+    subcategory_name = SelectField("Subcategory name:", choices=[], validators=[InputRequired()], validate_choice=False)
+    category_name = HiddenField("Category name:", validators=[DataRequired(), Length(max=100)])
+    submit = SubmitField("Submit")
